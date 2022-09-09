098f908cf67f181d36182574b899da82a8c8e6cd
islam-ahmed-github <112736634+islam-ahmed-github@users.noreply.github.com>
2022-09-09 22:18:14 +0200

    Update translator.py

--- final_project/translator.py
+++ final_project/translator.py
@@ -1,1 +1,24 @@
+import json
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
+# Set some variables
+api_key = '9gEW9sNI-hZIiE-7RFCCfiSETDHLZYn8ZfbokjriCpwj'
+api_url = 'https://api.us-south.language-translator.watson.cloud.ibm.com/instances/5236f27f-ae76-4f61-945e-88bc5533ca4b'
+ 
+
+
+
+# Prepare the Authenticator
+authenticator = IAMAuthenticator(api_key)
+language_translator = LanguageTranslatorV3( version='2018-05-01',authenticator=authenticator)
+language_translator.set_service_url(api_url )
+
+
+
+def Translate_en_to_fr(enstr):
+  return language_translator.translate(enstr,'fr-it').get_result()
+
+
+def Translate_fr_to_en(frstr):
+  return language_translator.translate(frstr,'en-it').get_result()
