bc30c1250baa4adc551189ffcbb0bb721e1652f7
islam-ahmed-github <112736634+islam-ahmed-github@users.noreply.github.com>
2022-09-09 22:37:56 +0200

    Update test_translator.py

--- final_project/test_translator.py
+++ final_project/test_translator.py
@@ -1,1 +1,16 @@
 
+import unittest
+from translator import  Translate_en_to_fr, Translate_fr_to_en
+
+class TestTranslator(unittest.TestCase):
+  def test_Translate_en_to_fr(self):
+    self.assertEqual(Translate_en_to_fr('hello'),'bonjour')
+    self.assertNotEqual(Translate_en_to_fr('hello'),'hello')
+    
+    
+  def test_Translate_en_to_fr(self):
+    self.assertEqual(Translate_fr_to_en('bonjour'),'hello')
+    self.assertNotEqual(Translate_fr_to_en('bonjour'),'bonjour')
+    
+if __name__=='__main__':
+  unittest.main()
